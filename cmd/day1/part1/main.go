fc2f660c4eeffb3aba9744510f4db91c30d89fb6
Tiarnán de Burca <tdeburca@gmail.com>
2022-05-10 16:00:40 +0100

    working day1 part1

--- cmd/day1/part1/main.go
+++ cmd/day1/part1/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"os"
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func check(e error) {
@@ -12,11 +14,27 @@ func check(e error) {
 }
 
 func main() {
-	s := "/home/tdeburca/git/AoC2022-go/cmd/day1/part1/testfile.txt"
-	dat, err := os.ReadFile(s)
+	// String for filename
+	// s := "/home/tdeburca/git/AoC2022-go/cmd/day1/part1/testfile.txt"
+	s := "/home/tdeburca/git/AoC2022-go/cmd/day1/part1/input.txt"
+	// assign filehandler to f.
+	f, err := os.Open(s)
 	check(err)
-	fmt.Print(string(dat))
-	fmt.Print("-----\n")
-
+	// create scanner for file
+	scanner := bufio.NewScanner(f)
+	// Get first number
+	scanner.Scan()
+	// convert returned text to integer.
+	current, _ := strconv.Atoi(scanner.Text())
+	// create counter
+	counter := 0
+	// Get all subsequent numbers
+	for scanner.Scan() {
+		next, _ := strconv.Atoi(scanner.Text())
+		if next > current {
+			counter++
+		}
+		current = next
+	}
+	fmt.Println(counter)
 }
-
