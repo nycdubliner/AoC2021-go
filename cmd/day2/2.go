c6adbc6852f66394f940cca87ac7e1791e14e3dd
Tiarnán de Burca <tdeburca@gmail.com>
2022-05-10 18:17:31 +0100

    day 2 complete

---
+++ cmd/day2/2.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	// String for filename
+	// s := "/home/tdeburca/git/AoC2021-go/cmd/day2/testfile.txt"
+	s := "/home/tdeburca/git/AoC2021-go/cmd/day2/input.txt"
+
+	// assign filehandler to f.
+	f, err := os.Open(s)
+	check(err)
+	// create scanner for file
+	scanner := bufio.NewScanner(f)
+
+	var input []string
+	for scanner.Scan() {
+		input = append(input, scanner.Text())
+	}
+
+	depth := 0
+	distance := 0
+	aim := 0
+
+	for _, direction := range input {
+		q := strings.Split(direction, " ")
+		if q[0] == "forward" {
+			vector, _ := strconv.Atoi(q[1])
+			distance = distance + vector
+			delta := aim * vector
+			depth = depth + delta
+		} else if q[0] == "down" {
+			vector, _ := strconv.Atoi(q[1])
+			aim = aim + vector
+		} else if q[0] == "up" {
+			vector, _ := strconv.Atoi(q[1])
+			aim = aim - vector
+		}
+
+	}
+	fmt.Print(depth*distance, "\n")
+
+}
